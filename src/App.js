251383a57517fd22b67c22eaa0653c889d9321fd
Sayandip Halder <sayandiph4@gmail.com>
2022-01-30 16:17:36 +0530

    delete everything to recreate a new design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React,  {useState} from 'react';
-import Buyer from './components/Buyer';
 
 function App() {
-  const [buyer, setBuyer] = useState(false);
-
-  if(buyer) return <Buyer />
   return (
-    <div className>
-      <button onClick={() => setBuyer(true)} >Log in as buyer</button>
+    <div >
+      
     </div>
   );
 }
